eb2892d449dadbfc74e90e821d69de1dd645fa4f
Paulo Tenório <paulorltenorio@gmail.com>
2026-01-10 15:52:33 -0300

    fix: avoid using ActivatedRoute before initialization

--- src/app/pages/meditacao/meditacao.page.ts
+++ src/app/pages/meditacao/meditacao.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { MEDITACOES } from '../../data/meditacoes';
+import { MEDITACOES } from '../../data/meditations';
 
 @Component({
   standalone: true,
@@ -8,8 +8,11 @@ import { MEDITACOES } from '../../data/meditacoes';
   templateUrl: './meditacao.page.html'
 })
 export default class MeditacaoPage {
-  id = this.route.snapshot.paramMap.get('id') ?? '';
-  m = MEDITACOES.find(x => x.id === this.id);
+  id = '';
+  m = MEDITACOES[0];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {
+    this.id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.m = MEDITACOES.find(x => x.id === this.id) ?? MEDITACOES[0];
+  }
 }
