66c5620d1cfd9c0c3450a55e8427b2dd9bddb965
Paulo Tenório <paulorltenorio@gmail.com>
2026-01-10 13:02:44 -0300

    feat: add lazy routes (home + detail)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/home/home.page') },
+  { path: 'meditacao/:id', loadComponent: () => import('./pages/meditacao/meditacao.page') },
+  { path: '**', redirectTo: '' }
+];
