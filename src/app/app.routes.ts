2bb58dca3077ae94b5063a1f1d2257380b23c73e
Paulo Tenório <paulorltenorio@gmail.com>
2026-01-12 02:10:38 -0300

    feat: add About page and navbar link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.page') },
   { path: 'meditacao/:id', loadComponent: () => import('./pages/meditacao/meditacao.page') },
+  { path: 'sobre', loadComponent: () => import('./pages/sobre/sobre.page') },
   { path: '**', redirectTo: '' }
+
 ];
