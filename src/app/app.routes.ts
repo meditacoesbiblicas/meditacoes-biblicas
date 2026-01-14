18baaa60f4b2d20b6d363b378dcccdec71180303
Paulo Tenório <paulorltenorio@gmail.com>
2026-01-14 01:01:00 -0300

    feat(ui): redesenha home (busca/chips/lista), adiciona Quem sou eu e refina páginas Sobre/Meditação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/home/home.page') },
-  { path: 'meditacao/:id', loadComponent: () => import('./pages/meditacao/meditacao.page') },
-  { path: 'sobre', loadComponent: () => import('./pages/sobre/sobre.page') },
+  { 
+    path: '',
+    loadComponent: () => import('./pages/home/home.page') },
+  { 
+    path: 'meditacao/:id',
+    loadComponent: () => import('./pages/meditacao/meditacao.page') },
+  { 
+    path: 'sobre',
+    loadComponent: () => import('./pages/sobre/sobre.page') },
+  {
+  path: 'quem-sou-eu',
+  loadComponent: () => import('./pages/quem-sou-eu/quem-sou-eu.page')
+  },
   { path: '**', redirectTo: '' }
 
 ];
